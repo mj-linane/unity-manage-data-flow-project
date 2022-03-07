72509605685e0cfae6e3a84b61b650955f0b12da
MJ <mjlinane@oldrochester.org>
2022-03-07 11:49:20 -0500

    Add exit functionality

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -26,5 +27,14 @@ public class MenuUIHandler : MonoBehaviour
     public void StartNew()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
