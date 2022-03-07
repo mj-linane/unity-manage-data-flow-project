2fb16d7ccc825cfec2e17f70e7d0de6357f1107a
MJ <mjlinane@oldrochester.org>
2022-03-07 12:09:05 -0500

    Add in TeamColor functionality

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static MainManager Instance;
+    public Color TeamColor;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
         
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 }
